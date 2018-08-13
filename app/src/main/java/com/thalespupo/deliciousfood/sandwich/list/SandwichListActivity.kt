7fc1c056436ca4154c6aa941a625bf04f107401f
Thales Pupo Araújo <thalespupo@gmail.com>
2018-08-13 09:37:53 -0300

    Added settings menu to wireup orders/promos activity
    Signed-off-by: Thales Pupo Araújo <thalespupo@gmail.com>

--- app/src/main/java/com/thalespupo/deliciousfood/sandwich/list/SandwichListActivity.kt
+++ app/src/main/java/com/thalespupo/deliciousfood/sandwich/list/SandwichListActivity.kt
@@ -2,13 +2,19 @@ package com.thalespupo.deliciousfood.sandwich.list
 
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
 import com.thalespupo.deliciousfood.R
 import com.thalespupo.deliciousfood.model.Sandwich
+import com.thalespupo.deliciousfood.order.OrderActivity
+import com.thalespupo.deliciousfood.promo.PromoActivity
 import com.thalespupo.deliciousfood.sandwich.SandwichActivity
 import kotlinx.android.synthetic.main.activity_sandwich_list.*
+
 
 class SandwichListActivity : AppCompatActivity() {
 
@@ -41,5 +47,25 @@ class SandwichListActivity : AppCompatActivity() {
     private fun onSandwichClicked(sandwich: Sandwich) {
         val intent = SandwichActivity.createIntent(this, sandwich)
         startActivity(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item!!.getItemId()) {
+            R.id.promos_item -> {
+                startActivity(Intent(this, PromoActivity::class.java))
+                true
+            }
+            R.id.orders_item -> {
+                startActivity(Intent(this, OrderActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
