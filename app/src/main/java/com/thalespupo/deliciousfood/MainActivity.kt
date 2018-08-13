b1b29fb6d0a23952041b6a0d2b95523136e197a3
Thales Pupo Araújo <thalespupo@gmail.com>
2018-08-13 09:17:34 -0300

    Setup splashscreen
    Signed-off-by: Thales Pupo Araújo <thalespupo@gmail.com>

--- app/src/main/java/com/thalespupo/deliciousfood/MainActivity.kt
+++ app/src/main/java/com/thalespupo/deliciousfood/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         val intent = Intent(this, SandwichListActivity::class.java)
         startActivity(intent)
